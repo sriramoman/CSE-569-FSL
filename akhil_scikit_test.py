58bd6e3c7c910e5d0017812a301314d13b08ad34
= <akhilravichandran11@gmail.com>
2016-12-05 00:27:08 -0700

    mc

--- akhil_scikit_test.py
+++ akhil_scikit_test.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
-
+import math
+import random
+import csv
+import traceback
 from sklearn import linear_model
+import numpy as np
 
 FILE = 'admission.csv'
 lines = csv.reader(open(FILE))
+data = list(lines)
+norm_x = np.array(data)[:,[0,1]]
+y_flip = np.array(data)[:,[2]]
+logreg = linear_model.LogisticRegression()
+logreg.fit(norm_x, y_flip)
+
+print logreg.predict(norm_x)
+

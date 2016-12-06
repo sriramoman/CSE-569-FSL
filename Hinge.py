f9690569496b4320da23a451ddc00f0ed7a28431
Sriram Vellangallor Subramanian <sriramoman@gmail.com>
2016-12-05 23:00:33 -0700

    Standardized code

--- Hinge.py
+++ Hinge.py
@@ -1,26 +1,31 @@
 import math
-import random
 import csv
 import traceback
+import random
 import numpy as np
+import sys
 
-momentum="nag"
-# momentum=None
-# momentum = "polyak"
-filename='rand.csv'
-iters=0
+try:
+    momentum = sys.argv[1]
+    print momentum
+except:
+    momentum = "polyak"
 
+iters = 0
 nfeatures = 10
-Wo=[1, 2, 3, 2, 5, 6, 7, 8, 9,10][:nfeatures]
-Wn=[0, 0, 0, 0, 0, 0, 0, 0, 0,0][:nfeatures]
+Wo = [1, 2, 3, 2, 5, 6, 7, 8, 9, 10][:nfeatures]
+Wn = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0][:nfeatures]
 predicted_thresh = 0.5
+thet = np.array((.1, .02, .3, .4, .5, .6, .3, .5, .2, .7))
+nita = 0.001
+thresh = 0.0001
+filename = 'rand.csv'
 
 
-def split_dataset(data, train_percentage):
+def read_csv(data, train_percentage):
     train = []
     test = list(data)
     train_size = int(train_percentage * len(data))
-
     while len(train) <= train_size:
         i = random.randrange(len(test))
         line = test.pop(i)
@@ -37,10 +42,10 @@ def fx(line, W):
     return sigmoid(fx)
 
 
-def predict(line, W):
-    predcited_value = fx(line,W)
-    expected_value = line[-1] if line[-1]==1 else 0
-    predicted_difference = np.abs(np.abs(expected_value)-predcited_value)
+def test_accuracy(line, W):
+    predcited_value = fx(line, W)
+    expected_value = line[-1] if line[-1] == 1 else 0
+    predicted_difference = np.abs(np.abs(expected_value) - predcited_value)
     if predicted_difference <= predicted_thresh:
         return 1
     else:
@@ -55,6 +60,7 @@ def sigma(train, W, i):
         else:
             sum += line[i] * (line[-1] - fx(line, W))
     return sum
+
 
 def rep(i, data):
     if i == (len(data) - 1):
@@ -72,94 +78,97 @@ def run(TRAIN_SIZE):
     FILE = filename
     lines = csv.reader(open(FILE))
     data = list(lines)
-    global nfeatures,Wo,Wn
+    global nfeatures, Wo, Wn
     nfeatures = len(data[0]) - 1
-    Wo=[Wo[:nfeatures]]
-    Wn=[Wn[:nfeatures]]
+    Wo = [Wo[:nfeatures]]
+    Wn = [Wn[:nfeatures]]
     for i in range(len(data)):
         data[i] = [rep(j, data[i]) for j in range(len(data[i]))]
-    # print data
-    train, test = split_dataset(data, TRAIN_SIZE)
+    train, test = read_csv(data, TRAIN_SIZE)
     # compute the hinge loss and assign it to W
     W = hinge(train)
     passed = 0
     # Validate predicted outcome against
     for line in test:
-        passed += predict(line, W)
+        passed += test_accuracy(line, W)
     print "Loss type: Hinge"
     print "Training Fraction: " + str(TRAIN_SIZE)
-    print "Iterations: "+str(iters)
+    print "Iterations: " + str(iters)
     print "Total Data Tested: " + str(len(test))
     print "Failed: " + str(len(test) - passed)
-    print "Pass: " + str(passed*100 / float(len(test))), "%"
-    print "Momentum: ",str(momentum)
+    print "Pass: " + str(passed * 100 / float(len(test))), "%"
+    print "Momentum: ", str(momentum)
     return 1.0 - (passed / float(len(test)))
 
 
 def hinge(training_data):
     all_data = np.array(training_data).astype(int)
-    all_x = all_data[:,range(0,np.shape(all_data)[1]-1)]
-    y = (all_data[:,-1]).T
-    return doTraning(all_x, y)
+    all_x = all_data[:, range(0, np.shape(all_data)[1] - 1)]
+    y = (all_data[:, -1]).T
+    return train_hinge(all_x, y)
 
 
-# http://stackoverflow.com/questions/3985619/how-to-calculate-a-logistic-sigmoid-function-in-python
 def sigmoid(x):
+    # referred http://stackoverflow.com/questions/3985619/how-to-calculate-a-logistic-sigmoid-function-in-python
     f = 1 / float(1 + math.exp(-x))
     return f
 
 
 def hinge_loss(w, x, y):
-    loss=0
-    grad=np.zeros((1,nfeatures))
+    loss = 0
+    grad = np.zeros((1, nfeatures))
     for (x_, y_) in zip(x, y):
         x_T = np.array([x_]).transpose()
         fx = np.dot(w, x_T)
         sig = sigmoid(fx)
         v = y_ * sig
         loss += max(0, 1 - v)
-        g = sig*(1 - sig)
-        zero_vectors =  np.zeros((1,nfeatures))
-        grad += (zero_vectors) if v > 1 else (-y_ *x_* g)
+        g = sig * (1 - sig)
+        zero_vectors = np.zeros((1, nfeatures))
+        grad += (zero_vectors) if v > 1 else (-y_ * x_ * g)
     return (loss, grad)
 
 
-def doTraning(x, y, thet=np.array((.1, .02, .3, .4, .5, .6, .3, .5, .2,.7)), nita=0.001, thresh=0.0001):
-    grad = np.inf
-    thet= np.array([thet[:nfeatures]])
-    ws = np.zeros((nfeatures, 0))
-    ws = np.hstack((ws, thet.reshape(nfeatures, 1)))
+def train_hinge(x, y):
+    #referred http://stats.stackexchange.com/questions/4608/gradient-of-hinge-loss
+    global thet
+    global nita
+    global thresh
+    thet = np.array([thet[:nfeatures]])
     ctr = 1
     delta = np.inf
     loss_prev = np.inf
-    v = np.ones((1,nfeatures))
-    u = 0 if momentum is None else 1
+    v = np.ones((1, nfeatures))
+    u = 0 if momentum is "none" or momentum is None else 1
     while np.abs(delta) > thresh:
-        feedback = u*v if momentum is 'nag' else 0
-        loss, grad = hinge_loss(thet+feedback, x, y)
+        #referred https://github.com/ragavvenkatesan/yann/tree/master/yann/core
+        feedback = u * v if momentum is 'nag' else 0
+        loss, grad = hinge_loss(thet + feedback, x, y)
         delta = loss_prev - loss
         loss_prev = loss
-        v = u*v - nita*grad
+        v = u * v - nita * grad
         thet = thet + v
         ctr += 1
     ws = thet[0]
     global iters
-    iters=ctr
-    print "Final Weight Vector:",ws
+    iters = ctr
+    print "Final Weight Vector:", ws
     return ws
 
 
-if __name__ == '__main__':
+def exec_hingeloss():
     try:
-        TRAIN_FRACTIONS = [.62]#, .62, .53, .625, 0.99]
-        avg = 1
-        accuracy_list = [0.0] * len(TRAIN_FRACTIONS)
-        for j in range(len(TRAIN_FRACTIONS)):
+        train_component = [.62]
+        average = 1
+        accuracy_list = [0.0] * len(train_component)
+        for j in range(len(train_component)):
             accuracy = 0.0
-            for i in range(avg):
-                accuracy += run(TRAIN_FRACTIONS[j])
-            accuracy /= avg
+            for i in range(average):
+                accuracy += run(train_component[j])
+            accuracy /= average
             accuracy_list[j] = accuracy
     except Exception as e:
         print e
         traceback.print_exc()
+
+exec_hingeloss()
---
+++ LogLikelihood.py
@@ -0,0 +1,130 @@
+import math
+import csv
+import traceback
+import random
+import sys
+
+try:
+    momentum = sys.argv[1]
+    print momentum
+except:
+    momentum = "nag"
+
+nfeatures = 9
+Wo = [0, 1, 2, 3, 2, 5, 6, 7, 8, 9, 1][:nfeatures]
+Wn = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0][:nfeatures]
+itr = 0
+
+predicted_thresh = 0.5
+filename = 'dataset.csv'
+
+def read_csv(data, train_percentage):
+    train = []
+    test = list(data)
+    train_size = int(train_percentage * len(data))
+
+    while len(train) <= train_size:
+        i = random.randrange(len(test))
+        line = test.pop(i)
+        train.append(line)
+    return [train, test]
+
+def log_loss(line, W):
+    fx = W[0]
+    for i in range(1, len(W) - 1):
+        fx += line[i] * W[i]
+    # referred http://stackoverflow.com/questions/3985619/how-to-calculate-a-logistic-sigmoid-function-in-python
+    result = round(math.exp(fx), 2) / round(((1 + math.exp(fx))), 2)
+    return result
+
+def test_accuracy(line, W):
+    result = log_loss(line, W)
+    if result >= predicted_thresh:
+        return 1
+    return 0
+
+def sigma(train, W, i, u, v):
+    sum = 0.0
+    total_loss = 0
+    for k in range(len(W)):
+        W[k] += u * v
+    for line in train:
+        loss = log_loss(line, W)
+        total_loss += loss
+        if i == 0:
+            sum += (line[-1] - loss)
+        else:
+            sum += line[i] * (line[-1] - loss)
+    return sum, total_loss
+
+def log_likelihood(train):
+    W_old = Wo
+    W_new = Wn
+    global itr
+    itr = 0
+    N = 0.01
+    v = 1
+    u = {}
+    u['polyak'] = 0
+    u['nag'] = .01
+    u[None] = 0
+    u["none"] = 0
+    feedback = u[momentum] if momentum is 'nag' else 0
+    while itr < 100:
+        for i in range(0, len(W_old)):
+            sig, loss = sigma(train, W_old, i, feedback, v)
+            v = u[momentum] * v + (N * sig)
+            W_new[i] = W_old[i] + v
+        W_old = W_new
+        itr += 1
+    return W_new
+
+
+def data_lookup(i, data):
+    if i == (len(data) - 1):
+        if data[i] == '2':
+            return 1
+        else:
+            return 0
+    x = data[i]
+    if x == '?' or x == '':
+        return 0
+    return int(x)
+
+def train(training_portion):
+    FILE = filename
+    lines = csv.reader(open(FILE))
+    data = list(lines)
+    for i in range(len(data)):
+        data[i] = [data_lookup(j, data[i]) for j in range(len(data[i]))]
+    train, test = read_csv(data, training_portion)
+    weights = log_likelihood(train)
+    passed = 0
+    for entry in test:
+        if entry[-1] == test_accuracy(entry, weights):
+            passed += 1
+    print "Loss type: Log Likelihood"
+    print "Training Fraction: " + str(training_portion)
+    print "Iterations: " + str(itr)
+    print "Total Data Tested: " + str(len(test))
+    print "Failed: " + str(len(test) - passed)
+    print "Pass: " + str(passed * 100 / float(len(test))), "%"
+    print "Momentum: ", str(momentum)
+    return 1.0 - (passed / float(len(test)))
+
+def exec_loglikelihood():
+    try:
+        train_component = [.35]
+        average = 1
+        accuracy_list = [0.0] * len(train_component)
+        for j in range(len(train_component)):
+            accuracy = 0.0
+            for i in range(average):
+                accuracy += train(train_component[j])
+            accuracy /= average
+            accuracy_list[j] = accuracy
+    except Exception as e:
+        print e
+        traceback.print_exc()
+
+exec_loglikelihood()
